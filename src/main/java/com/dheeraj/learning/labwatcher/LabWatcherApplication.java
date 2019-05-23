d3a64405537c562162f9ddefa4114953010f3862
Dheeraj Kumar Gopali <DheerajKumar.Gopali@in.pega.com>
2019-05-23 16:15:27 +0530

    Perfectly working thread related code. Yet to fine tune the logic to increase readability of the code.

--- src/main/java/com/dheeraj/learning/labwatcher/LabWatcherApplication.java
+++ src/main/java/com/dheeraj/learning/labwatcher/LabWatcherApplication.java
@@ -2,12 +2,29 @@ package com.dheeraj.learning.labwatcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class LabWatcherApplication {
     public static void main(String[] args) {
         SpringApplication.run(LabWatcherApplication.class, args);
+    }
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(20);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("ThreadService-");
+        executor.initialize();
+        return executor;
     }
 }
